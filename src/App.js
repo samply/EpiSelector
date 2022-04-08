3048f0b6cd9e01b8f3575c05181a9d10a32b6c9a
na12 <n.al-hasnawi@dkfz-heidelberg.de>
2022-04-08 13:47:23 +0200

    First structure of Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import NavB from './NavB'
+import Mainpage from './Mainpage';
+import Betapage from './Betapage';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import InfoBox from "./InfoBox";
+import InputProtokoll from "./InputProtokoll";
+import TopNav from "./TopNav";
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Router>
+          <TopNav/>
+        <div className="App">
+          <NavB/>
+          <Switch>
+            <Route path='/' exact component={Mainpage}/>
+            <Route path="/mainpage" component={Mainpage}/>
+            <Route path="/betapage" component={Betapage}/>
+          </Switch>
+            <div className="rightSideBuild">
+            <InputProtokoll/>
+            <InfoBox/>
+            </div>
+        </div>
+      </Router>
   );
 }
 
