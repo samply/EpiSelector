12324a43f2f6934b390ba04365ad90030bba313d
Nabe Al Hasnawi <n.al-hasnawi@dkfz-heidelberg.de>
2022-06-13 17:05:39 +0200

    Created Constructor for File.js

---
+++ src/model/File.js
@@ -0,0 +1,12 @@
+export class Form {
+
+    constructor(dataSource, dataFile, matchingMethod, matchingVariables, matchingToleranz, casesAndControll, matchingmeasure) {
+        this.dataSource = dataSource;
+        this.dataFile = dataFile;
+        this.matchingMethod = matchingMethod;
+        this.matchingVariables = matchingVariables;
+        this.matchingToleranz = matchingToleranz;
+        this.casesAndControll = casesAndControll;
+        this.matchingmeasures = matchingmeasure;
+    }
+}
