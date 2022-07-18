c617ac7a82654ed39f117243bcad5ecd3e63cea4
E260-NB08\ms9 <monique.stenzel@medma.uni-heidelberg.de>
2022-07-18 13:38:50 +0200

    balance diagnostics

--- statisticalapi/plumber_matchit_nnm.R
+++ statisticalapi/plumber_matchit_nnm.R
@@ -10,6 +10,10 @@
 library(plumber)
 library ("MatchIt")
 library(rjson)
+library("ggplot2")
+library("cobalt")
+library("lmtest") #coeftest
+library("sandwich") #vcovCL
 
 #* @apiTitle Plumber Example API
 #* @apiDescription Plumber example description.
@@ -91,6 +95,46 @@ function(req,res,groupindicator,controllvariables, mdistance, mmethod, mreplace,
   # return(back_to_JSON)
   res$body <- back_to_JSON
   res
+  
+  
+# #  balance diagnostics #############################################################################################################
+#     # Checking balance prior to matching
+#   summary(body)
+#   
+#   # Checking balance after matching
+#   summary(m.out1, un = FALSE)
+#   # un:	logical; whether to compute balance statistics for the unmatched sample. Default TRUE; set to FALSE for more concise output.
+#   
+#   bal.tab(m.out1, un = TRUE, stats = c("m", "v", "ks"))
+#   # setting stats = c("m", "v", "ks") we requested mean differences, variance ratios, and Kolmogorov-Smirnov statistics
+#   # Kolmogorov-Smirnov: p-Wert kleiner als 0,05 -> signifikante Abweichung von der Normalverteilung / Daten nicht normalverteilt sind
+#   # graphische Überprüfung der Normalverteilung anhand Quantil-Quantil-plots Q-Q-plots
+#   # Q-Q-Plots: Gegenüberstellung der theoretischen Quantile, welche die Daten aufweisen sollten, wenn sie perfekt normalverteilt wären, und der Quantilen der Messwerte 
+#   # Wenn die Daten perfekt normalverteilt wären, würden alle Punkte auf der Line liegen
+#   
+#   # Love plot
+#   love.plot(m.out1, binary = "std")
+#   
+#   # Density plot 
+#   bal.plot(m.out1, var.name = "controllvariables")
+#   
+#   bal.plot(m.out1, var.name = "controllvariables")
+  
+  # Mirrored histogram
+  bal.plot(m.out1, var.name = controllvariables[3], 
+           type = "histogram", mirror = TRUE)
+  
+  # grouped boxplots
+  # 
+  # #eQQ plot
+  # plot(m.out1, type = "qq", which.xs = c(controllvariables[3]))
+  # 
+  # # t test of coefficients
+  # # noch anpassen
+  # #fit1 <- lm(re78 ~ treat + age + educ + race + married + nodegree + 
+  #  #            re74 + re75, data = result_dataset, weights = weights)
+  # 
+  # #coeftest(fit1, vcov. = vcovCL, cluster = ~subclass)
 }
 
 
